6e0f3af66c3930a34054923f0f8f4b00b7a9dba4
Ayan  Dutta <ayandutta.it@gmail.com>
2023-05-28 13:31:06 +0530

    Create incNumTriangle.java

---
+++ incNumTriangle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class incNumTriangle{
+    public static void main(String args[]) {
+    
+    //Question 6
+        //Print the pattern below
+        //1
+        //12
+        //123       
+        //1234
+        //12345
+        //int a=0;
+        //int i=1;
+        while (i<=10) {
+            for (int j=1;j<=i;j++) {
+                System.out.print(a+1);
+                a=a+1;
+            }
+           
+            i++;
+            System.out.println();
+            a=0;
+        }
+    }
+}
